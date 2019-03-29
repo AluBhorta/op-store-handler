7aa2f2f54767b5e343e4be83ad60eef07d1651c5
AluBhorta <dev.bhorta@gmail.com>
2019-03-29 13:24:02 +0600

    temp commit

--- client/src/components/order/AddNewOrderPage.js
+++ client/src/components/order/AddNewOrderPage.js
@@ -2,10 +2,31 @@ import React, { Component } from "react";
 const { ipcRenderer } = window.require("electron");
 
 export default class AddNewOrderPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <div>
         <h1>Add New Order</h1>
+        <form>
+          <div>
+            <input placeholder="name" />
+          </div>
+          <div>
+            <button name="submit" onClick={this.handleSubmit}>
+              Submit
+            </button>
+            <button name="cancel" onClick={this.handleCancel}>
+              Cancel
+            </button>
+            <button name="clear" onClick={this.handleClear}>
+              Clear
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
